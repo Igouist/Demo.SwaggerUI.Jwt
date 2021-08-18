71aea5b6e8831cd731f48d33dd33c40dd0c16ec2
Igouist <Ursaliker@gmail.com>
2021-08-19 07:58:17 +0800

    Ref: 移除不必要的內容

---
+++ JwtDemo/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JwtDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
